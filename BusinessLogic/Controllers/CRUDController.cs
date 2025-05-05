2d93c18d1fbc440c20b259cd46af126348a627c5
Alo080701 <rosette.louise@hotmail.dk>
2025-05-05 20:40:09 +0200

    Business logik CRUD og anvendelige metoder

--- BusinessLogic/Controllers/CRUDController.cs
+++ BusinessLogic/Controllers/CRUDController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTO.Model;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BusinessLogic.Controllers
 {
@@ -10,24 +12,24 @@ namespace BusinessLogic.Controllers
     {
 
         //CRUD Reol
-        public static void OpretNyReol()
+        public static void OpretNyReol(string reolId, int pladserBred, int pladserHoej)
+        {
+            
+        }
+
+        public static void RedigerReol(Reol reol)
         {
 
         }
 
-        public static void RedigerReol()
-        {
-
-        }
-
-        public static void SletReol()
+        public static void SletReol(Reol reol)
         {
 
         }
 
         //CRUD PLADS
 
-        public static void OpretPlads()
+        public static void OpretPlads(Varegruppe varegruppe, Reol reol, int pladsX, int pladsY)
         {
 
         }
@@ -45,6 +47,18 @@ namespace BusinessLogic.Controllers
 
         //CRUD VARE
 
+        public static void OpretVare(int vareId, long ean, string model, Varegruppe varegruppe, string note)
+        {
+            if(ean.ToString().Length != 13)
+            {
+                throw new Exception("fejl");
+            }
+            else
+            {
+
+            }
+
+        }
 
 
     }
---
+++ BusinessLogic/Controllers/funktionsMetoder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO.Model;
+
+namespace BusinessLogic.Controllers
+{
+    public class funktionsMetoder
+    {
+        public static void findPlads(Vare vare)
+        {
+
+        }
+    }
+}
