5825eb14b7c2368da914ea8e847480467982bebc
Alo080701 <rosette.louise@hotmail.dk>
2025-05-05 13:17:50 +0200

    Business layer CRUD

---
+++ BusinessLogic/Controllers/CRUDController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Controllers
+{
+    public class CRUDController
+    {
+
+        //CRUD Reol
+        public static void OpretNyReol()
+        {
+
+        }
+
+        public static void RedigerReol()
+        {
+
+        }
+
+        public static void SletReol()
+        {
+
+        }
+
+        //CRUD PLADS
+
+        public static void OpretPlads()
+        {
+
+        }
+
+        public static void RedigerPlads()
+        {
+
+        }
+
+        public static void SletPlads()
+        {
+
+        }
+
+
+        //CRUD VARE
+
+
+
+    }
+}
