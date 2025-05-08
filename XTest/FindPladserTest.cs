d2ab006895454b4939affe432d9f17b7c6a561a2
DanielKongen <Danielkonge1@Gmail.com>
2025-05-08 19:22:47 +0200

    Tilføjet sidste test. Speacial bliver fikset i morgen

--- XTest/FindPladserTest.cs
+++ XTest/FindPladserTest.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 namespace XTest
 {
-    public class UnitTest1
+    public class FindPladserTest
     {
         [Fact]
         public void FindPladserTilVareStandard()
@@ -26,7 +26,7 @@ namespace XTest
         [Fact]
         public void FindPladserTilVareHoej()
         {
-            //standard vare opvasker
+            //Hoej vare vinskab
             Vare vareTest = LagerRepository.GetVare(18);
             List<Plads> pladser = funktionsMetoder.FindPladserTilVare(vareTest.Varegruppe);
             List<int> forventetPladsId = [1, 2, 3, 23, 24, 25, 26, 28, 50, 51, 52, 53, 54, 55, 56];
@@ -41,10 +41,29 @@ namespace XTest
         [Fact]
         public void FindPladserTilVareSpecial()
         {
-            //standard vare opvasker
+            //Special vare kummefryser
             Vare vareTest = LagerRepository.GetVare(3);
             List<Plads> pladser = funktionsMetoder.FindPladserTilVare(vareTest.Varegruppe);
             List<int> forventetPladsId = [1, 2, 3, 23, 24, 25, 26, 28, 50, 51, 52, 53, 54, 55, 56];
+            //Tjekker for hver plads om den er der
+            foreach (int i in forventetPladsId)
+            {
+                Plads plads = CRUDController.HentPlads(i);
+                //Assert.Contains(plads, pladser);
+                Assert.Contains(plads, pladser);
+            }
+        }
+
+        [Fact]
+        public void FindPladserTilVareMedIndsatVare()
+        {
+            //standard vare opvasker
+            Vare vareTest = LagerRepository.GetVare(12);
+
+            Vare nyVareIndsat = new Vare(16, 733254399653, "BFS8500T", Varegruppe.Standard);
+
+            List<Plads> pladser = funktionsMetoder.FindPladserTilVare(vareTest.Varegruppe);
+            List<int> forventetPladsId = [1, 2, 3, 8, 11, 12, 13, 14, 15, 17, 18, 19, 20, 23, 24, 25, 26, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56];
             //Tjekker for hver plads om den er der
             foreach (int i in forventetPladsId)
             {
